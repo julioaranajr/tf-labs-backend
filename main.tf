25a324ba4b2565b258d7069b7b0624b8af0aa2b2
Julio Arana Jr <julio.arana@cloudreach.com>
2022-10-11 15:44:10 +0200

    Creating New Backend for Talent-Academy

---
+++ main.tf
@@ -0,0 +1,40 @@
+# RESOURCE "RESOURCE_TYPE" "RESOURCE_NAME"
+resource "aws_s3_bucket" "ta_backend_bucket" {
+  bucket = "ta-vpc-terraform-tfstates-342055123193"
+
+  # META-DATA
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name        = "ta-vpc-terraform-tfstates-342055123193"
+    Environment = "Test"
+    Team        = "Talent-Academy"
+    Owner       = "Julio-Arana-Jr"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "version_my_bucket" {
+  bucket = aws_s3_bucket.ta_backend_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock_tbl" {
+  name           = "terraform-lock"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags           = {
+    Name = "terraform-lock"
+  }
+}
