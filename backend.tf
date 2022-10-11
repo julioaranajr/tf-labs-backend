7a759d433372a028cb067d55df84cd9eca7143a8
Julio Arana Jr <113112001+julioaranajr@users.noreply.github.com>
2022-10-11 17:00:59 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
     bucket = "ta-labs-cicd-terraform-tfstates-342055123193"
-    key    = "sprint1/week2/training-cicd/terraform.tfstates"
+    key    = "sprint1/week6/training-cicd/terraform.tfstates"
   }
 }
